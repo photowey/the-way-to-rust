7d9937a5f4796f726c573517070a5c8caf36a56a
photowey <photowey@gmail.com>
2024-02-12 15:09:59 +0800

    feat: Add: 1.add guessing_game module
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ 02_guessing_game/src/main.rs
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    // rand
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+        // new: associated function
+        let mut guess = String::new();
+
+        // &: reference
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Shadowing
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
