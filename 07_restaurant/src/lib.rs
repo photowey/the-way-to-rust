81bfdc9c9fd7d0d47b2213312518884038a67434
photowey <photowey@gmail.com>
2024-02-15 16:44:06 +0800

    feat: Update: 1.restaurant module
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ 07_restaurant/src/lib.rs
@@ -0,0 +1,119 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+
+crate
+ └── front_of_house
+     ├── hosting
+     │   ├── add_to_waitlist
+     │   └── seat_at_table
+     └── serving
+         ├── take_order
+         ├── serve_order
+         └── take_payment
+
+*/
+
+use crate::front_of_house::hosting;
+
+mod front_of_house {
+    // Note: `pub`
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+// ----------------------------------------------------------------
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+// ----------------------------------------------------------------
+
+/*
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+}
+*/
+
+// re-exporting
+// pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    hosting::add_to_waitlist();
+}
+
+// ----------------------------------------------------------------
+
+mod customer {
+    // use crate::front_of_house::hosting;
+    use crate::front_of_house::hosting::add_to_waitlist;
+
+    pub fn eat_at_restaurant() {
+        // hosting::add_to_waitlist();
+
+        add_to_waitlist();
+    }
+}
